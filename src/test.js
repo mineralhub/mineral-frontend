148fb80a8460ca9a2d6c96d2eee27e32c056e196
RyoungHo <chl4745@naver.com>
2018-10-29 21:27:01 +0900

    rename function.

--- src/test.js
+++ src/test.js
@@ -13,7 +13,7 @@ const Int64 = require('int64-buffer').Int64LE;
 
 let addr = '12AKRNHpFhDSBDD9rSn74VAzZSL3774PxQ';
 
-function BaseUnitTest(type, txdata) {
+function baseUnitTest(type, txdata) {
   console.log('----- transaction base unit test : ' + type);
   console.log(txdata.toBuffer());
   console.log(txdata.toBuffer().length);
@@ -23,31 +23,31 @@ function BaseUnitTest(type, txdata) {
 let base = new TransactionBase();
 base.fee = new Int64(0);
 base.from = addr;
-BaseUnitTest('base', base);
+baseUnitTest('base', base);
 
 let reward = new RewardTransaction();
 reward.fee = new Int64(0);
 reward.from = addr;
 reward.reward = new Int64(25000000000);
-BaseUnitTest('reward', reward);
+baseUnitTest('reward', reward);
 
 let transfer = new TransferTransaction();
 transfer.fee = new Int64(0);
 transfer.from = addr;
 transfer.to[addr] = new Int64(10000000);
-BaseUnitTest('trasfer', transfer);
+baseUnitTest('trasfer', transfer);
 
 let vote = new VoteTransaction();
 vote.fee = new Int64(0);
 vote.from = addr;
 vote.votes[addr] = new Int64(10000000);
-BaseUnitTest('vote', vote);
+baseUnitTest('vote', vote);
 
 let delegate = new RegisterDelegateTransaction();
 delegate.fee = new Int64(0);
 delegate.from = addr;
 delegate.name = Buffer.from('one');
-BaseUnitTest('delegate', delegate);
+baseUnitTest('delegate', delegate);
 
 let otherSign = new OtherSignTransaction();
 otherSign.fee = new Int64(0);
@@ -55,24 +55,24 @@ otherSign.from = addr;
 otherSign.to[addr] = new Int64(1000);
 otherSign.others.push(addr);
 otherSign.expirationBlockHeight = 100000;
-BaseUnitTest('otherSign', otherSign);
+baseUnitTest('otherSign', otherSign);
 
 let sign = new SignTransaction();
 sign.fee = new Int64(0);
 sign.from = addr;
 sign.signTxHash = Buffer.alloc(32);
-BaseUnitTest('sign', sign);
+baseUnitTest('sign', sign);
 
 let lock = new LockTransaction();
 lock.fee = new Int64(0);
 lock.from = addr;
 lock.lockValue = new Int64(1000000);
-BaseUnitTest('lock', lock);
+baseUnitTest('lock', lock);
 
 let unlock = new UnlockTransaction();
 unlock.fee = new Int64(0);
 unlock.from = addr;
-BaseUnitTest('unlock', unlock);
+baseUnitTest('unlock', unlock);
 
 console.log('----- transaction unit test');
 let transaction = new Transaction(TransactionType.Transfer);
