e4e2b8fd2de175cc8a6404b9839969116b1e8262
RyoungHo <chl4745@naver.com>
2018-10-29 20:00:30 +0900

    add. transactions class & test

--- src/common/Transaction.js
+++ src/common/Transaction.js
@@ -1,5 +1,6 @@
-const Int64 = require('int64-buffer').Int64BE;
+const Int64 = require('int64-buffer').Int64LE;
 const base58Check = require('bs58check');
+const EC = require('elliptic').ec;
 
 var TransactionType = {
   None: 0,
@@ -24,7 +25,7 @@ var addrToHash = (addr) => {
 
 class TransactionBase {
   constructor() {
-    this.fee = new Int64(0); // 8 byte (Fixed8)
+    this.fee = undefined; // 8 byte (Fixed8)
     this.from = undefined; // 20 byte (UInt160 address hash)
   }
 
@@ -43,7 +44,7 @@ class TransactionBase {
 class RewardTransaction extends TransactionBase {
   constructor() {
     super();
-    this.reward = new Int64(); // 8 byte (Fixed8)
+    this.reward = undefined; // 8 byte (Fixed8)
   }
 
   size() {
@@ -80,24 +81,185 @@ class TransferTransaction extends TransactionBase {
   }
 }
 
+class VoteTransaction extends TransactionBase {
+  constructor() {
+    super();
+    this.votes = {}; // key: 20 byte (UInt160 address hash), value : 8 byte (Fixed8) = 28 * n
+  }
+
+  size() {
+    return super.size() + Object.keys(this.votes).length * 28;
+  }
+
+  toBuffer() {
+    let buf = Buffer.alloc(this.size(), super.toBuffer());
+    let cursor = super.size();
+    for (let addr in this.votes) {
+      addrToHash(addr).copy(buf, cursor);
+      cursor += 20;
+      this.votes[addr].toBuffer().copy(buf, cursor);
+      cursor += 8;
+    }
+    return buf;
+  }
+}
+
+class RegisterDelegateTransaction extends TransactionBase {
+  constructor() {
+    super();
+    this.name = undefined; // dynamic size (byte array)
+  }
+  
+  size() {
+    return super.size() + this.name.length;
+  }
+
+  toBuffer() {
+    let buf = Buffer.alloc(this.size(), super.toBuffer());
+    this.name.copy(buf, super.size());
+    return buf;
+  }
+}
+
+class OtherSignTransaction extends TransactionBase {
+  constructor() {
+    super();
+    this.to = {}; // key: 20 byte (UInt160 address hash), value : 8 byte (Fixed8) = 28 * n
+    this.others = []; // dynamic size (string array)
+    this.expirationBlockHeight = 0; // 4 byte (integer)
+  }
+
+  othersSize() {
+    let size = 0;
+    for (let i = 0; i < this.others.length; ++i) {
+      size += this.others[i].length;
+    }
+    return size;
+  }
+
+  size() {
+    return super.size() + Object.keys(this.to).length * 28 + this.othersSize() + 4;
+  }
+
+  toBuffer() {
+    let buf = Buffer.alloc(this.size(), super.toBuffer());
+    let cursor = super.size();
+    for (let addr in this.to) {
+      addrToHash(addr).copy(buf, cursor);
+      cursor += 20;
+      this.to[addr].toBuffer().copy(buf, cursor);
+      cursor += 8;      
+    }
+    for (let i = 0; i < this.others.length; ++i) {
+      buf.write(this.others[i], cursor);
+      cursor += this.others[i].length;
+    }
+    buf.writeInt32LE(this.expirationBlockHeight, cursor);
+    return buf;
+  }
+}
+
+class SignTransaction extends TransactionBase {
+  constructor() {
+    super();
+    this.signTxHash = undefined; // 32 byte (UInt256)
+  }
+
+  size() {
+    return super.size() + 32;
+  }
+
+  toBuffer() {
+    let buf = Buffer.alloc(this.size(), super.toBuffer());
+    this.signTxHash.copy(buf, super.size());
+    return buf;
+  }
+}
+
+class LockTransaction extends TransactionBase {
+  constructor() {
+    super();
+    this.lockValue = undefined; // 8 byte (Fixed8)
+  }
+
+  size() {
+    return super.size() + 8;
+  }
+
+  toBuffer() {
+    let buf = Buffer.alloc(this.size(), super.toBuffer());
+    this.lockValue.toBuffer().copy(buf, super.size());
+    return buf;
+  }
+}
+
+class UnlockTransaction extends TransactionBase {
+  constructor() {
+    super();
+  }
+
+  size() {
+    return super.size();
+  }
+
+  toBuffer() {
+    return super.toBuffer();
+  }
+}
+
 class Transaction {
   constructor(type = TransactionType.None) {
-    this.version = 0;
-    this.type = type;
-    this.timestamp = 0;
-    this.data = undefined;
+    this.version = 0; // 2 byte (short)
+    this.type = type; // 2 byte (short)
+    this.timestamp = 0; // 4 byte (integer)
+    this.data = undefined; // dynamic size
     this.signature = undefined;
     this.pubkey = undefined;
   }
 
-  toBuffer() {
+  size() {
+    return 2 + 2 + 4 + this.data.size() + this.signature.length + this.pubkey.length;
+  }
 
+  sizeUnsigned() {
+    return 2 + 2 + 4 + this.data.size();
+  }
+
+  toBuffer() {
+    let buf = Buffer.alloc(this.size(), this.toBufferUnsigned());
+    this.signature.copy(buf, this.sizeUnsigned());
+    this.pubkey.copy(buf, this.sizeUnsigned() + this.signature.length);
+    return buf;
+  }
+
+  toBufferUnsigned() {
+    let buf = Buffer.alloc(this.sizeUnsigned());
+    buf.writeInt16LE(this.version, 0);
+    buf.writeInt16LE(this.type, 2);
+    buf.writeInt32LE(this.timestamp, 4);
+    this.data.toBuffer().copy(buf, 8);
+    return buf;
+  }
+
+  sign(prikey) {
+    let bufUnsigned = this.toBufferUnsigned();
+    let ec = new EC('secp256k1');
+    let key = ec.keyFromPrivate(prikey, 'hex');
+    this.signature = Buffer.from(ec.sign(bufUnsigned, key).toDER(), 'hex');
+    this.pubkey = Buffer.from(key.getPublic().encode('hex'), 'hex');
   }
 }
 
 module.exports = {
+  TransactionType,
   TransactionBase,
   RewardTransaction,
   TransferTransaction,
+  VoteTransaction,
+  RegisterDelegateTransaction,
+  OtherSignTransaction,
+  SignTransaction,
+  LockTransaction,
+  UnlockTransaction,
   Transaction
 }
--- src/test.js
+++ src/test.js
@@ -1,24 +1,81 @@
-const {RewardTransaction, TransferTransaction, TransactionBase, Transaction} = require("./common/Transaction");
-const Int64 = require('int64-buffer').Int64BE;
+const {RewardTransaction, 
+  TransferTransaction, 
+  VoteTransaction,
+  RegisterDelegateTransaction,
+  OtherSignTransaction,
+  SignTransaction,
+  LockTransaction,
+  UnlockTransaction,
+  TransactionBase, 
+  Transaction, 
+  TransactionType} = require("./common/Transaction");
+const Int64 = require('int64-buffer').Int64LE;
 
 let addr = '12AKRNHpFhDSBDD9rSn74VAzZSL3774PxQ';
 
-function UnitTest(type, txdata) {
-  console.log('----- unit test : ' + type);
+function BaseUnitTest(type, txdata) {
+  console.log('----- transaction base unit test : ' + type);
   console.log(txdata.toBuffer());
   console.log(txdata.toBuffer().length);
   console.log(txdata.toBuffer().length === txdata.size());
 }
 
 let base = new TransactionBase();
+base.fee = new Int64(0);
 base.from = addr;
-UnitTest('base', base);
+BaseUnitTest('base', base);
 
 let reward = new RewardTransaction();
+reward.fee = new Int64(0);
 reward.from = addr;
-UnitTest('reward', reward);
+reward.reward = new Int64(25000000000);
+BaseUnitTest('reward', reward);
 
 let transfer = new TransferTransaction();
+transfer.fee = new Int64(0);
 transfer.from = addr;
 transfer.to[addr] = new Int64(10000000);
-UnitTest('trasfer', transfer);
+BaseUnitTest('trasfer', transfer);
+
+let vote = new VoteTransaction();
+vote.fee = new Int64(0);
+vote.from = addr;
+vote.votes[addr] = new Int64(10000000);
+BaseUnitTest('vote', vote);
+
+let delegate = new RegisterDelegateTransaction();
+delegate.fee = new Int64(0);
+delegate.from = addr;
+delegate.name = Buffer.from('one');
+BaseUnitTest('delegate', delegate);
+
+let otherSign = new OtherSignTransaction();
+otherSign.fee = new Int64(0);
+otherSign.from = addr;
+otherSign.to[addr] = new Int64(1000);
+otherSign.others.push(addr);
+otherSign.expirationBlockHeight = 100000;
+BaseUnitTest('otherSign', otherSign);
+
+let sign = new SignTransaction();
+sign.fee = new Int64(0);
+sign.from = addr;
+sign.signTxHash = Buffer.alloc(32);
+BaseUnitTest('sign', sign);
+
+let lock = new LockTransaction();
+lock.fee = new Int64(0);
+lock.from = addr;
+lock.lockValue = new Int64(1000000);
+BaseUnitTest('lock', lock);
+
+let unlock = new UnlockTransaction();
+unlock.fee = new Int64(0);
+unlock.from = addr;
+BaseUnitTest('unlock', unlock);
+
+console.log('----- transaction unit test');
+let transaction = new Transaction(TransactionType.Transfer);
+transaction.data = transfer;
+transaction.sign('6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b');
+console.log(transaction.toBuffer().length === transaction.size());
